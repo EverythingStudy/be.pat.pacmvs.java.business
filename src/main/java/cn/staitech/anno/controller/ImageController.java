6ecf201180404e284c0cf66e39048efa9e9dc194
wangfeng <wangf@staitech.cn>
2023-09-11 09:42:21 +0800

    排查分页

--- src/main/java/cn/staitech/anno/controller/ImageController.java
+++ src/main/java/cn/staitech/anno/controller/ImageController.java
@@ -52,6 +52,7 @@ public class ImageController extends BaseController {
     @Log(title = "查询切片列表", menu = "切片管理", subMenu = "原始切片", businessType = BusinessType.QUERY)
     @PostMapping("/list")
     public R<PageMaster<ImageListOutVO>> list(@Validated @RequestBody ImageListVO image) throws ExecutionException, InterruptedException {
+        log.info("----------ImageListVO:{}",image);
         image.setBusinessType(1);
         PageMaster<ImageListOutVO> page = imageService.selectList(image);
         return R.ok(page);
