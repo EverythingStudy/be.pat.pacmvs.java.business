0f0bc2d79a9c18f2e80c86f7cf468ef0e04924ce
wangfeng <wangf@staitech.cn>
2023-09-18 18:17:00 +0800

    评审轮次

---
+++ src/main/java/cn/staitech/anno/controller/ReviewRoundController.java
@@ -0,0 +1,78 @@
+package cn.staitech.anno.controller;
+
+import cn.staitech.anno.domain.ReviewRound;
+import cn.staitech.anno.domain.reviewround.ReviewRoundInVO;
+import cn.staitech.anno.service.ReviewRoundService;
+import cn.staitech.anno.utils.PageMaster;
+import cn.staitech.common.core.domain.R;
+import cn.staitech.common.security.utils.SecurityUtils;
+import cn.staitech.system.api.domain.SysUser;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.github.pagehelper.PageHelper;
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * @author: wangfeng
+ * @create: 2023-09-18 17:15:58
+ * @Description: 评审轮次
+ */
+@Api(value = "评审轮次", tags = "评审轮次")
+@RestController
+@RequestMapping("/reviewRound")
+@Slf4j
+public class ReviewRoundController {
+
+    @Resource
+    private ReviewRoundService reviewRoundService;
+
+    @ApiOperationSupport(author = "wangfeng")
+    @ApiOperation(value = "查询评审轮次列表")
+    @GetMapping("/list")
+    public R<PageMaster<List<ReviewRound>>> list(@NotNull(message = "分页参数为空！") @RequestParam("pageNum") @ApiParam(name = "pageNum", value = "分页参数", required = true) Integer pageNum,
+                                                 @NotNull(message = "分页参数为空！") @RequestParam("pageSize") @ApiParam(name = "pageSize", value = "分页参数", required = true) Integer pageSize,
+                                                 @RequestParam("reviewRoundId") Long reviewRoundId) {
+        PageHelper.startPage(pageNum, pageSize).setReasonable(true);
+        ReviewRound reviewRound = new ReviewRound();
+        reviewRound.setReviewRoundId(reviewRoundId);
+        QueryWrapper queryWrapper = new QueryWrapper<>(reviewRound);
+        List<ReviewRound> list = reviewRoundService.list(queryWrapper);
+        PageMaster pageMaster = new PageMaster<>(list);
+        return R.ok(pageMaster);
+    }
+
+    @ApiOperationSupport(author = "wangfeng")
+    @ApiOperation(value = "添加评审轮次")
+    @PostMapping("/add")
+    public R add(@RequestBody ReviewRoundInVO reviewRoundInVO) {
+        ReviewRound reviewRound = new ReviewRound();
+        BeanUtils.copyProperties(reviewRoundInVO, reviewRound);
+        SysUser sysUser = SecurityUtils.getLoginUser().getSysUser();
+        reviewRound.setCreateBy(sysUser.getUserId());
+        reviewRound.setOrganizationId(sysUser.getOrganizationId());
+        return R.ok(reviewRoundService.save(reviewRound));
+    }
+
+    @ApiOperationSupport(author = "wangfeng")
+    @ApiOperation(value = "批量删除评审轮次")
+    @GetMapping(value = "/remove")
+    public R remove(@RequestParam("reviewRoundIds") List<Long> reviewRoundIds) {
+        return R.ok(reviewRoundService.removeByIds(reviewRoundIds));
+    }
+
+    @ApiOperationSupport(author = "wangfeng")
+    @ApiOperation(value = "修改评审轮次")
+    @PostMapping("/edit")
+    public R edit(@RequestBody ReviewRound reviewRound) {
+        return R.ok(reviewRoundService.save(reviewRound));
+    }
+}
--- src/main/java/cn/staitech/anno/domain/reviewround/ReviewRoundInVO.java
+++ src/main/java/cn/staitech/anno/domain/reviewround/ReviewRoundInVO.java
@@ -1,20 +1,15 @@
-package cn.staitech.anno.domain;
+package cn.staitech.anno.domain.reviewround;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
-import java.io.Serializable;
-import java.util.Date;
-
 /**
+ * @author wangf
  * @TableName tb_review_round
  */
-@TableName(value = "tb_review_round")
 @Data
-public class ReviewRound extends Pager implements Serializable {
+public class ReviewRoundInVO {
     /**
      * 评审轮次自增ID
      */
@@ -50,27 +45,4 @@ public class ReviewRound extends Pager implements Serializable {
      * 机构ID
      */
     private Long organizationId;
-
-    /**
-     * 创建者
-     */
-    private Long createBy;
-
-    /**
-     * 更新者
-     */
-    private Long updateBy;
-
-    /**
-     * 创建时间
-     */
-    private Date createTime;
-
-    /**
-     * 更新时间
-     */
-    private Date updateTime;
-
-    @TableField(exist = false)
-    private static final long serialVersionUID = 1L;
 }
