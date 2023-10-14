cc75c7f4290c1d60d24e86005b64972a5fd7ac0c
geng'jiang'tao <gjt123456>
2023-10-14 20:05:01 +0800

    更新考核文件内容

--- src/main/java/cn/staitech/anno/service/impl/ExamineScoreServiceImpl.java
+++ src/main/java/cn/staitech/anno/service/impl/ExamineScoreServiceImpl.java
@@ -3,17 +3,27 @@ package cn.staitech.anno.service.impl;
 import cn.hutool.core.date.DateUtil;
 import cn.staitech.anno.domain.*;
 import cn.staitech.anno.domain.examineScore.*;
+import cn.staitech.anno.domain.geojson.*;
 import cn.staitech.anno.mapper.*;
 import cn.staitech.anno.project.domain.Project;
+import cn.staitech.anno.project.domain.Slide;
+import cn.staitech.anno.project.mapper.MarkingMapperV1;
 import cn.staitech.anno.project.mapper.ProjectMapperV1;
+import cn.staitech.anno.project.mapper.SlideMapperV1;
+import cn.staitech.anno.project.service.SlideAttrService;
 import cn.staitech.anno.queue.DelayQueueExample;
 import cn.staitech.anno.service.ExamineScoreService;
+import cn.staitech.anno.service.FileService;
 import cn.staitech.anno.service.MarkingService;
 import cn.staitech.anno.utils.ExcludeEmptyQueryWrapper;
 import cn.staitech.anno.utils.MessageSource;
+import cn.staitech.anno.utils.RandomUtils;
 import cn.staitech.common.core.domain.PageResponse;
 import cn.staitech.common.security.utils.SecurityUtils;
 import cn.staitech.system.api.RemoteLabelService;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson2.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -23,9 +33,10 @@ import com.ibm.icu.text.SimpleDateFormat;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -53,16 +64,25 @@ public class ExamineScoreServiceImpl extends ServiceImpl<ExamineScoreMapper, Exa
     private RemoteLabelService remoteLabelService;
 
     @Resource
+    private PathologicalIndicatorCategoryMapper pathologicalIndicatorCategoryMapper;
+
+    @Resource
+    private MarkingMapper markingMapper;
+
+    @Resource
+    private SlideMapperV1 slideMapperV1;
+
+    @Resource
     private ProjectMarksRelMapper projectMarksRelMapper;
 
     @Resource
     private DelayQueueExample delayQueueExample;
 
     @Resource
-    private MarkingService markingService;
+    private ProjectMapperV1 projectMapperV1;
 
     @Resource
-    private ProjectMapperV1 projectMapperV1;
+    private FileService fileService;
 
     @Resource
     private QuestionProjectRelMapper questionProjectRelMapper;
@@ -220,7 +240,7 @@ public class ExamineScoreServiceImpl extends ServiceImpl<ExamineScoreMapper, Exa
         examineScore.setUpdateTime(sdf.format(date));
         String fileUrl;
         try {
-            fileUrl = markingService.slideJsonExport(examineScoreBy.getSlideId());
+            fileUrl = slideJsonExport(examineScoreBy);
         } catch (Exception exception) {
             log.error(exception.toString());
             throw new RuntimeException(MessageSource.M("ERROR_HAS_ALREADY"));
@@ -236,6 +256,136 @@ public class ExamineScoreServiceImpl extends ServiceImpl<ExamineScoreMapper, Exa
         // 更新当前评分记录
         return res;
     }
+
+    public String slideJsonExport(ExamineScore examineScoreBy) throws Exception {
+        Long slideId = examineScoreBy.getSlideId();
+        if (!Optional.ofNullable(slideId).isPresent()) {
+            try {
+                throw new Exception("参数异常");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        Slide slideBy = slideMapperV1.selectById(slideId);
+        if (!Optional.ofNullable(slideBy).isPresent()) {
+            try {
+                throw new Exception("未查询到切片信息");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        String fileUrl = null;
+        try {
+            fileUrl = fileService.createFiles(slideId, ".json");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        // 标注数据
+        MarkingExamine markingExamine = new MarkingExamine();
+        markingExamine.setQuestionProjectId(examineScoreBy.getQuestionProjectId());
+        markingExamine.setCreateBy(examineScoreBy.getCreateBy());
+        List<Features> features = markingExamineMapper.selectLists(markingExamine);
+        features.forEach(i -> i.setGeometry(updateY(i.getGeometry())));
+
+        // 查询项目详情
+        JsonExport jsonExport = null;
+        Project projectBy = projectMapperV1.selectById(slideBy.getProjectId());
+        if (Objects.equals(projectBy.getProjectType(), "2")) {
+            jsonExport = markingMapper.jsonExportSelect(slideId);
+        } else {
+            jsonExport = markingMapper.reviewJsonExportSelect(slideId);
+        }
+
+        // 项目信息
+        GeoProject project = new GeoProject();
+        // 种属编码 + 结构编码 + 数据库项目id
+        String projectId = jsonExport.getSpeciesId() + "_" + jsonExport.getOrganId() + "_" + jsonExport.getProjectId();
+        project.setProject_id(projectId);
+        project.setProject_name(jsonExport.getProjectName());
+
+        // 图像信息
+        GeoImage image = new GeoImage();
+        image.setImage_shape(jsonExport.getImageShape());
+        image.setImage_type(jsonExport.getFormat());
+        image.setImage_name(jsonExport.getImageName());
+        image.setCreate_time(jsonExport.getCreateTime());
+        // 项目id + 十三位时间戳 + 两位随机数
+        String imageId = jsonExport.getProjectId() + "_" + System.currentTimeMillis() + "_" + RandomUtils.RandomNumbers();
+        image.setImage_id(imageId);
+        image.setImage_url(jsonExport.getImageUrl());
+
+        // 作者信息
+        GeoAttribute attribute = new GeoAttribute();
+        attribute.setAuthor(SecurityUtils.getLoginUser().getSysUser().getUserName());
+        attribute.setDepartment(SecurityUtils.getLoginUser().getSysUser().getDept());
+
+        // 标签信息
+        QueryWrapper<MarkingExamine> markingQueryWrapper = new QueryWrapper<>();
+        markingQueryWrapper
+                .select("category_id")
+                .eq("question_project_id",examineScoreBy.getQuestionProjectId())
+                .eq("create_by",examineScoreBy.getCreateBy())
+                .ne("category_id",0)
+                .groupBy("category_id");
+        List<MarkingExamine> markingList = markingExamineMapper.selectList(markingQueryWrapper);
+        List<GeoLabel> categoryList = new ArrayList<>();
+        if(markingList.size() > 0){
+            for(MarkingExamine marking:markingList){
+                GeoLabel geoLabel = pathologicalIndicatorCategoryMapper.selectGeoLabel(marking.getCategoryId());
+                categoryList.add(geoLabel);
+            }
+        }
+
+        // 构建geoJson数据
+        GeoJson geoJson = new GeoJson();
+        geoJson.setFeatures(features);
+        geoJson.setImage(image);
+        geoJson.setProject(project);
+        geoJson.setAttribute(attribute);
+        geoJson.setLabel_info(categoryList);
+        String jsonString = JSON.toJSONString(geoJson, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue);
+        // 写入文件
+        exportJson(fileUrl, jsonString);
+//            return R.ok(fileUrl);
+//        });
+        return fileUrl;
+    }
+
+    public static com.alibaba.fastjson.JSONObject updateY(com.alibaba.fastjson.JSONObject geometry) {
+        List<Object> lists = new ArrayList<>();
+        JSONArray coordinatesJsonArray1 = geometry.getJSONArray("coordinates");
+        String type = geometry.getString("type");
+        List<Object> list1 = new ArrayList<>();
+        for(Object i1: coordinatesJsonArray1){
+            JSONArray jsonArray1 = JSONArray.parseArray(i1.toString());
+            for(Object i2:jsonArray1){
+                JSONArray jsonArray2 = (JSONArray) i2;
+                List<Double> list = com.alibaba.fastjson.JSONObject.parseArray(jsonArray2.toJSONString(),Double.class);
+                List<Double> newList = new ArrayList<>();
+                newList.add(list.get(0));
+                newList.add(Double.valueOf(String.valueOf(Math.abs(list.get(1)))));
+                list1.add(newList);
+            }
+        }
+        lists.add(list1);
+        com.alibaba.fastjson.JSONObject geometryJson = new com.alibaba.fastjson.JSONObject();
+        geometryJson.put("type",type);
+        geometryJson.put("coordinates",lists);
+        return geometryJson;
+    }
+
+    public void exportJson(String fileUrl, String jsonString) {
+        try {
+            OutputStream outputStream = Files.newOutputStream(Paths.get(fileUrl));
+            outputStream.write(jsonString.getBytes());
+            // 关闭流
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     public void updatePersonalFit(Long examineScoreId) {
