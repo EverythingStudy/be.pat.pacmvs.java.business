aac129af5105373390a16a6e680d791416cf6d97
gengjt <gengjt@staitech.cn>
2023-10-20 17:47:47 +0800

    接口修改1.4

---
+++ src/main/java/cn/staitech/anno/controller/SysUserController.java
@@ -0,0 +1,34 @@
+package cn.staitech.anno.controller;
+
+import cn.staitech.anno.domain.user.in.SelectUserIdListIN;
+import cn.staitech.anno.project.mapper.SysUserMapperV1;
+import cn.staitech.common.core.domain.R;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Api(value = "用户配置", tags = "用户配置")
+@RestController
+@RequestMapping("/user")
+public class SysUserController {
+
+    @Resource
+    private SysUserMapperV1 userMapper;
+
+
+    @ApiOperation(value = "根据用户id查询用户列表")
+    @PostMapping("/selectUserIdList")
+    public R<List<cn.staitech.anno.project.domain.SysUser>> selectUserIdList(@RequestBody SelectUserIdListIN req) {
+        QueryWrapper<cn.staitech.anno.project.domain.SysUser> sysUserQueryWrapper = new QueryWrapper<>();
+        sysUserQueryWrapper.in("user_id",req.getUserIdList()).eq("del_flag","0");
+        return R.ok(userMapper.selectList(sysUserQueryWrapper));
+    }
+}
---
+++ src/main/java/cn/staitech/anno/domain/user/in/SelectUserIdListIN.java
@@ -0,0 +1,13 @@
+package cn.staitech.anno.domain.user.in;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SelectUserIdListIN {
+
+    @ApiModelProperty(value = "用户列表")
+    private List<Long> userIdList;
+}
