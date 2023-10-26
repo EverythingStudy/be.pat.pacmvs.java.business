426f729532b4bd16cd1a896d884adfb200bf2210
gengjt <gengjt@staitech.cn>
2023-10-26 16:14:39 +0800

    新增项目类型接口

---
+++ src/main/java/cn/staitech/anno/service/AlgorithmProjectTypeService.java
@@ -0,0 +1,16 @@
+package cn.staitech.anno.service;
+
+import cn.staitech.anno.domain.AlgorithmProjectType;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author gjt
+ * @since 2023-10-26
+ */
+public interface AlgorithmProjectTypeService extends IService<AlgorithmProjectType> {
+
+}
