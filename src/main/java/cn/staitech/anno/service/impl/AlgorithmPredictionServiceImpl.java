fa335e07a6da54586d62419937eda05e6eab5fa6
wanglibei <wanglb@staitech.cn>
2023-11-14 18:22:46 +0800

    sql优化

--- src/main/java/cn/staitech/anno/service/impl/AlgorithmPredictionServiceImpl.java
+++ src/main/java/cn/staitech/anno/service/impl/AlgorithmPredictionServiceImpl.java
@@ -236,8 +236,7 @@ public class AlgorithmPredictionServiceImpl implements AlgorithmPredictionServic
 		ImageCsvGetVO imageCsvGetVO = new ImageCsvGetVO();
 		BeanUtil.copyProperties(request, imageCsvGetVO);
 		List<ImageCsvListVO> list = slidePredictionMapper.getImageCsvListVOList(imageCsvGetVO);
-		//
-		for(ImageCsvListVO vo:list){
+		/*for(ImageCsvListVO vo:list){
 			EyeThumImageQuery query = new EyeThumImageQuery();
 			Long slideId = vo.getSlideId();
 			query.setSlideId(slideId);
@@ -247,7 +246,7 @@ public class AlgorithmPredictionServiceImpl implements AlgorithmPredictionServic
 			if(CollectionUtils.isNotEmpty(mainImageList)){
 				vo.setPredictionThumbUrl(mainImageList.get(0).getThumbUrl());
 			}
-		}
+		}*/
 		PageMaster<ImageCsvListVO> pageMaster = new PageMaster<>(list);
 		PageHelper.clearPage();
 		return pageMaster;
