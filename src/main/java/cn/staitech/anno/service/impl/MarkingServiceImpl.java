09394710deca023425018092b84a1bf60421c6ce
gengjt <gengjt@staitech.cn>
2023-11-20 16:44:50 +0800

    标注数据修改异常修复1.2

--- src/main/java/cn/staitech/anno/service/impl/MarkingServiceImpl.java
+++ src/main/java/cn/staitech/anno/service/impl/MarkingServiceImpl.java
@@ -297,9 +297,11 @@ public class MarkingServiceImpl implements MarkingService {
             marking.setPerimeter(String.valueOf(perimeter));
         }
         List<PointCount> pointCountList = updatePoint(markingBy.getLocation_type(), markingBy);
-        if (req.getGeometry().isEmpty()) {
-            log.info("标注数据异常:" + req.getGeometry() + "------------------------------------------------->");
-            throw new Exception("更新失败，轮廓数据不能为空");
+        if(req.getGeometry() != null){
+            if (req.getGeometry().isEmpty()) {
+                log.info("标注数据异常:" + req.getGeometry() + "------------------------------------------------->");
+                throw new Exception("更新失败，轮廓数据不能为空");
+            }
         }
         markingMapper.updateById(marking);
         // 判断标签
