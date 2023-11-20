3bdb7f4a2c0a536ea48d3e75482467e9ec7ede70
gengjt <gengjt@staitech.cn>
2023-11-20 16:46:52 +0800

    标注修改丢失数据异常修复1.1

--- src/main/java/cn/staitech/anno/service/impl/MarkingServiceImpl.java
+++ src/main/java/cn/staitech/anno/service/impl/MarkingServiceImpl.java
@@ -238,9 +238,11 @@ public class MarkingServiceImpl implements MarkingService {
             marking.setImage_url(image.getImageUrl());
         }
         marking.setNumber(number);
-        if (req.getGeometry() == null || req.getGeometry().isEmpty()) {
-            log.info("标注数据异常:" + req.getGeometry() + "------------------------------------------------->");
-            throw new Exception("更新失败，轮廓数据不能为空");
+        if(req.getGeometry() != null){
+            if (req.getGeometry().isEmpty()) {
+                log.info("标注数据异常:" + req.getGeometry() + "------------------------------------------------->");
+                throw new Exception("更新失败，轮廓数据不能为空");
+            }
         }
         // 添加数据库，添加后返回自增id
         markingMapper.insert(marking);
@@ -297,9 +299,11 @@ public class MarkingServiceImpl implements MarkingService {
             marking.setPerimeter(String.valueOf(perimeter));
         }
         List<PointCount> pointCountList = updatePoint(markingBy.getLocation_type(), markingBy);
-        if (req.getGeometry() == null || req.getGeometry().isEmpty()) {
-            log.info("标注数据异常:" + req.getGeometry() + "------------------------------------------------->");
-            throw new Exception("更新失败，轮廓数据不能为空");
+        if(req.getGeometry() != null){
+            if (req.getGeometry().isEmpty()) {
+                log.info("标注数据异常:" + req.getGeometry() + "------------------------------------------------->");
+                throw new Exception("更新失败，轮廓数据不能为空");
+            }
         }
         markingMapper.updateById(marking);
         // 判断标签
