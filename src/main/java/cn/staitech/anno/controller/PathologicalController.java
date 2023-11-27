f61f8788cd70878d3d14565e135e136acd80f90c
wanglibei <wanglb@staitech.cn>
2023-11-27 18:07:27 +0800

    标签add

--- src/main/java/cn/staitech/anno/controller/PathologicalController.java
+++ src/main/java/cn/staitech/anno/controller/PathologicalController.java
@@ -90,8 +90,8 @@ public class PathologicalController {
 			return R.fail(MessageSource.M("INDICATOR_ABSENT"));
 		}
 		//验证当前脏器下是否已经超过30个标签（颜色只有30种） 
-		int labelCount = pathologicalIndicatorCategoryService.selectLabelNumByStructureId(structureId);
-		if(labelCount > 30){
+		Integer labelCount = pathologicalIndicatorCategoryService.selectLabelNumByStructureId(structureId);
+		if(null != labelCount && labelCount > 30){
 			return R.fail(MessageSource.M("CATEGORY_NAME_EXIST"));
 		}
 		//验证结构是否已经存在 
