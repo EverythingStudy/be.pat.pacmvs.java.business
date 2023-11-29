665db1185c88a96301171bab62600a79bb08a542
wanglibei <wanglb@staitech.cn>
2023-11-29 13:59:28 +0800

    历史数据处理

---
+++ src/main/java/cn/staitech/anno/config/ScheduleConfig.java
@@ -0,0 +1,19 @@
+package cn.staitech.anno.config;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 读取定时任务相关配置
+ *
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "schedule")
+public class ScheduleConfig {
+    private String cron;
+    
+}
+
