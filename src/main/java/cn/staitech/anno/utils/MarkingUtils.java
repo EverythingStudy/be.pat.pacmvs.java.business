5c3790ee9fbb5c46b0209c7aef67ebe1e6b7cc8a
gengjt <gengjt@staitech.cn>
2023-12-08 16:34:05 +0800

    合并后轮廓面积计算1.0

--- src/main/java/cn/staitech/anno/utils/MarkingUtils.java
+++ src/main/java/cn/staitech/anno/utils/MarkingUtils.java
@@ -189,9 +189,9 @@ public class MarkingUtils {
                 // 获取geometry类型
                 String geometryType = geometry.getGeometryType();
                 // TODO：校验飞点 后续可写多种校验策略放入线程池中
-                if ("Polygon".equals(geometryType)) {
-                    geometry = removePolygonPoint(geometry.getCoordinates());
-                }
+//                if ("Polygon".equals(geometryType)) {
+//                    geometry = removePolygonPoint(geometry.getCoordinates());
+//                }
 
                 // 判断新图形是否为复杂多边型(比如大标注嵌套小标注
                 if ("MultiPolygon".equals(geometryType)) {
