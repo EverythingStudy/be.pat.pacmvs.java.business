127c58b0d4c4b49ac796d5d0607d56ed7b51d16b
wangfeng <wangf@staitech.cn>
2023-12-13 13:28:55 +0800

    专题-删除delFlag属性

--- src/main/java/cn/staitech/anno/controller/TopicController.java
+++ src/main/java/cn/staitech/anno/controller/TopicController.java
@@ -45,7 +45,6 @@ public class TopicController {
         QueryWrapper<Topic> qw = new QueryWrapper();
         qw.eq(!SysUser.isAdmin(SecurityUtils.getUserId()),"organization_id", organizationId)
                 .eq("project_type_id", projectTypeId)
-                .eq("del_flag", 1)
                 .orderByDesc("topic_id");
         List<Topic> list = topicService.list(qw);
         return R.ok(list);
@@ -74,7 +73,6 @@ public class TopicController {
                 .topicName(req.getTopicName().trim().toString())
                 .createBy(uid)
                 .updateBy(uid)
-                .delFlag(1)
                 .createTime(time)
                 .updateTime(time)
                 .build();
