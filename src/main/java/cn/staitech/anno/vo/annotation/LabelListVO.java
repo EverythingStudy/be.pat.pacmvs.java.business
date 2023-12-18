13786263a8d8149abb249805dd3b2b20e701dd0a
wangfeng <wangf@staitech.cn>
2023-12-18 16:42:13 +0800

    自定义标签

--- src/main/java/cn/staitech/anno/vo/annotation/LabelListVO.java
+++ src/main/java/cn/staitech/anno/vo/annotation/LabelListVO.java
@@ -64,5 +64,7 @@ public class LabelListVO {
     private String type;
     @ApiModelProperty(value = "组内排序 1:标注区域 2：考核区域 3：结构标签")
     private Integer groupNumber;
+    @ApiModelProperty(value = "标签类型 0:下拉筛选标签；1:自定义标签")
+    private Integer categoryType;
     
 }
