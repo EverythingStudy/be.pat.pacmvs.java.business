62937d27ff5580e7c87362a7513da9113c1e9c02
wanglibei <wanglb@staitech.cn>
2023-12-20 17:18:23 +0800

    结构编码增加校验

--- src/main/java/cn/staitech/anno/controller/PathologicalController.java
+++ src/main/java/cn/staitech/anno/controller/PathologicalController.java
@@ -139,12 +139,12 @@ public class PathologicalController {
 			String speciesId = indicator.getSpeciesId();
 			String organId = indicator.getOrganId();
 			
-			QueryWrapper<Structure> querySidWrapper = new QueryWrapper<>();
+			/*QueryWrapper<Structure> querySidWrapper = new QueryWrapper<>();
 			querySidWrapper.eq("structure_id",structureId);
 			List<Map<String, Object>> sIdRList = structureService.listMaps(querySidWrapper);
 			if(CollectionUtils.isNotEmpty(sIdRList)){
 				return R.fail("STRUCTUREID EXIST");
-			}
+			}*/
 			
 			
 			QueryWrapper<Structure> queryWrapper = new QueryWrapper<>();
