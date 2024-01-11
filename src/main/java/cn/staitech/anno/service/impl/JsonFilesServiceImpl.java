9aa2b769893556039bbf5a9f67389e8df25baad6
gengjt <gengjt@staitech.cn>
2024-01-11 14:26:23 +0800

    上传文件异常修复1.2

--- src/main/java/cn/staitech/anno/service/impl/JsonFilesServiceImpl.java
+++ src/main/java/cn/staitech/anno/service/impl/JsonFilesServiceImpl.java
@@ -56,51 +56,59 @@ public class JsonFilesServiceImpl implements JsonFilesService {
 
     @Override
     public String mergeChunk(FileUploadVO chunk) throws Exception {
+        // 拼接路径
         String path = basePath + File.separator + OrganizationUtils.geNumber(SecurityUtils.getLoginUser().getSysUser().getOrganizationId()) + zipPath + File.separator + chunk.getFileName();
         File file = new File(path);
         if (file.exists()) {
             // 删除文件
             if (file.delete()) {
                 // 创建文件
-                if (file.mkdirs()) {
-                    // 将文件数量和文件id添加至map中
-                    if (!Container.FILE_MAP.containsKey(chunk.getUuid())) {
-                        ConcurrentHashSet<Integer> chunkSet = new ConcurrentHashSet<Integer>(chunk.getChunkTotal());
-                        for (int i = 0; i < chunk.getChunkTotal(); i++) {
-                            chunkSet.add(i);
-                        }
-                        Container.FILE_MAP.put(chunk.getUuid(), chunkSet);
-                    }
-                    try (InputStream fis = chunk.getMultipartFile().getInputStream(); RandomAccessFile raf = new RandomAccessFile(file, "rw")) {
-                        int len = -1;
-                        byte[] buffer = new byte[1024 * 4 * 10];
-                        // 指针移动到当前块开始写的位置，chunk.getChunkNumber()是指当前是第几块，减一后乘
-                        // 以每个块的大小 得到前面块的偏移量，即当前块的起始位置
-                        raf.seek((chunk.getChunk()) * chunk.getChunkSize());
-                        // 写入文件
-                        while ((len = fis.read(buffer)) != -1) {
-                            raf.write(buffer, 0, len);
-                        }
-                    } catch (IOException e) {
-                        log.error("合并文件异常");
-                        return "0";
-                    }
-                    // 删除SET中当前元素
-                    if (Container.FILE_MAP.containsKey(chunk.getUuid()) && StringUtils.isNotEmpty(chunk.getUuid())) {
-                        Container.FILE_MAP.get(chunk.getUuid()).remove(chunk.getChunk());
-                    }
-                    // 删除ConcurrentHashMap中对应ConcurrentHashSet - map为空时,代表文件上传完成,根据业务类型执行不同业务
-                    if (Container.FILE_MAP.containsKey(chunk.getUuid()) && Container.FILE_MAP.get(chunk.getUuid()).isEmpty()) {
-                        // map中删除当前文件信息
-                        Container.FILE_MAP.remove(chunk.getUuid());
-                        parseJson(chunk.getProjectId(), path);
-                    }
-                } else {
-                    log.error("创建文件异常");
-                }
+                mergeFile(chunk, path, file);
             } else {
                 log.error("删除文件异常");
             }
+        }else {
+            mergeFile(chunk, path, file);
+        }
+        return "1";
+    }
+
+    public String mergeFile(FileUploadVO chunk, String path, File file) throws Exception {
+        if (file.mkdirs()) {
+            // 将文件数量和文件id添加至map中
+            if (!Container.FILE_MAP.containsKey(chunk.getUuid())) {
+                ConcurrentHashSet<Integer> chunkSet = new ConcurrentHashSet<Integer>(chunk.getChunkTotal());
+                for (int i = 0; i < chunk.getChunkTotal(); i++) {
+                    chunkSet.add(i);
+                }
+                Container.FILE_MAP.put(chunk.getUuid(), chunkSet);
+            }
+            try (InputStream fis = chunk.getMultipartFile().getInputStream(); RandomAccessFile raf = new RandomAccessFile(file, "rw")) {
+                int len = -1;
+                byte[] buffer = new byte[1024 * 4 * 10];
+                // 指针移动到当前块开始写的位置，chunk.getChunkNumber()是指当前是第几块，减一后乘
+                // 以每个块的大小 得到前面块的偏移量，即当前块的起始位置
+                raf.seek((chunk.getChunk()) * chunk.getChunkSize());
+                // 写入文件
+                while ((len = fis.read(buffer)) != -1) {
+                    raf.write(buffer, 0, len);
+                }
+            } catch (IOException e) {
+                log.error("合并文件异常");
+                return "0";
+            }
+            // 删除SET中当前元素
+            if (Container.FILE_MAP.containsKey(chunk.getUuid()) && StringUtils.isNotEmpty(chunk.getUuid())) {
+                Container.FILE_MAP.get(chunk.getUuid()).remove(chunk.getChunk());
+            }
+            // 删除ConcurrentHashMap中对应ConcurrentHashSet - map为空时,代表文件上传完成,根据业务类型执行不同业务
+            if (Container.FILE_MAP.containsKey(chunk.getUuid()) && Container.FILE_MAP.get(chunk.getUuid()).isEmpty()) {
+                // map中删除当前文件信息
+                Container.FILE_MAP.remove(chunk.getUuid());
+                parseJson(chunk.getProjectId(), path);
+            }
+        } else {
+            log.error("创建文件异常");
         }
         return "1";
     }
