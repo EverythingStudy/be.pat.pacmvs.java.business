9b07c10d3591a027cc2f4b4b28df610bc1e51883
wanglibei <wanglb@staitech.cn>
2024-01-18 16:47:58 +0800

    server-disk-size_0.2

--- src/main/java/cn/staitech/anno/controller/UserManualController.java
+++ src/main/java/cn/staitech/anno/controller/UserManualController.java
@@ -8,11 +8,19 @@ import cn.staitech.common.security.utils.SecurityUtils;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -65,6 +73,52 @@ public class UserManualController {
         }
 
         return R.ok(userManual);
+    }
+    
+    @GetMapping("/getServerInfo")
+    public R getServerInfo() {
+    	/*File[] files = File.listRoots();
+    	for (File file : files) {
+    		String total = new DecimalFormat("#.#").format(file.getTotalSpace() * 1.0 / 1024 / 1024 / 1024) + "G";
+    		String free = new DecimalFormat("#.#").format(file.getFreeSpace() * 1.0 / 1024 / 1024 / 1024) + "G";
+    		String un = new DecimalFormat("#.#").format(file.getUsableSpace() * 1.0 / 1024 / 1024 / 1024) + "G";
+    		String path = file.getPath();
+    		System.out.println(path + "总:" + total + ",可用空间:" + un + ",空闲空间:" + free);
+    		System.out.println("=============================================");
+    	}*/
+
+//    	String pathAll = "";
+//    	String allTotal = execBash(pathAll);
+//    	System.out.println("allTotal："+allTotal);
+    	
+    	String onlyPath = "/home/staitech";
+    	String onlyPathTotal = execBash(onlyPath);
+    	System.out.println("onlyPathTotal："+onlyPathTotal);
+    	return R.ok();
+    }
+    
+    private String execBash(String path){
+    	String result = "";
+    	String cmd = "df -h ";
+    	if(StringUtils.isNotEmpty(path)){
+    		cmd = cmd +path;
+    	}
+    	System.out.println("cmd-2："+cmd);
+    	StringBuffer sb = new StringBuffer();
+    	try {
+    		Process process;
+    		process = Runtime.getRuntime().exec(cmd);
+
+    		BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    		String line;
+    		while ((line = br.readLine()) != null) {
+    			sb.append(line).append("\n");
+    		}
+    		 result = sb.toString();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	return result;
     }
 
 }
