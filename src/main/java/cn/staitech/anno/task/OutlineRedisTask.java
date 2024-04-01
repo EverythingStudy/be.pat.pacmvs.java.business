d5da7a642f0ace789fe01bd7a527b4dc9baaa3f1
wangfeng <wangf@staitech.cn>
2024-04-01 14:23:44 +0800

    撤消还原-优化

--- src/main/java/cn/staitech/anno/task/OutlineRedisTask.java
+++ src/main/java/cn/staitech/anno/task/OutlineRedisTask.java
@@ -6,6 +6,7 @@ import cn.staitech.anno.service.impl.HistoryServiceImpl;
 import cn.staitech.anno.utils.RocksDBUtil;
 import cn.staitech.common.core.constant.CacheConstants;
 import cn.staitech.common.redis.service.RedisService;
+import io.vertx.core.impl.ConcurrentHashSet;
 import lombok.extern.slf4j.Slf4j;
 import org.rocksdb.RocksDBException;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +21,7 @@ import java.util.Map;
 
 import static cn.staitech.anno.constant.CommonConstant.REDIS_OUTLINE_LIST;
 import static cn.staitech.anno.constant.CommonConstant.REDIS_OUTLINE_ROOT;
+import static cn.staitech.anno.utils.RocksDBUtil.USER_ROCKS_MAP;
 
 /**
  * 清空token失效后Redis对应的数据：上一次执行完毕时间点后30秒再次执行
@@ -47,12 +49,16 @@ public class OutlineRedisTask {
 
         // 获取token有效用户(在线用户)数据，map:<userId, token>
         Map<Long, String> loginMap = new HashMap<>(16);
+        ConcurrentHashSet<Long> loginHashSet = new ConcurrentHashSet<>();
+
         Collection<String> loginKeyCollection = redisService.keys(CacheConstants.LOGIN_TOKEN_KEY + "*");
         for (String loginKey : loginKeyCollection) {
             if (loginKey.length() == 49) {
                 String token = loginKey.replaceFirst(CacheConstants.LOGIN_TOKEN_KEY, "");
                 cn.staitech.system.api.model.LoginUser loginUser = redisService.getCacheObject(loginKey);
                 loginMap.put(loginUser.getUserid(), token);
+                loginHashSet.add(loginUser.getUserid());
+                log.info("loginMap: {}:{}", loginUser.getUserid(), token);
             }
         }
 
@@ -78,22 +84,36 @@ public class OutlineRedisTask {
             }
         }
 
-        // 删除token无效对应数据:遍历撤消、恢复历史记录
+        // 删除token无效对应数据:遍历撤消、恢复历史记录 v2
+        for (Map.Entry<Long, ConcurrentHashSet<String>> entry : USER_ROCKS_MAP.entrySet()) {
+            log.info("[USER_SESSION_MAP]删除token失效用户rocksdb数据:{}", entry);
+            Long key = entry.getKey();
+            if (!loginHashSet.contains(key)) {
+                for (String triceId : entry.getValue()) {
+                    try {
+                        RocksDBUtil.cfDeleteIfExist(triceId);
+                    } catch (RocksDBException e) {
+                        log.info("[USER_SESSION_MAP]定时任务删除废弃rocksdb数据:{},{}", triceId, e);
+                    }
+                }
+                USER_ROCKS_MAP.remove(key);
+            }
+        }
+
+
+        // 删除token无效对应数据:遍历撤消、恢复历史记录 v1
         for (Map.Entry<String, Session> entry : HistoryServiceImpl.USER_SESSION_MAP.entrySet()) {
-
-//            log.info("****************************************************\n");
-//            log.info("entry:{}", entry);
-
+            log.info("[USER_SESSION_MAP]删除token失效用户rocksdb数据:{}", entry);
             String key = entry.getKey();
             Long userId = Long.valueOf(key.split("_")[0]);
-            if (!loginMap.containsKey(userId)) {
+            if (!loginHashSet.contains(userId)) {
                 Session session = entry.getValue();
                 LinkedList<Trace> tracesList = session.getDrawList();
                 for (Trace trace : tracesList) {
                     try {
                         RocksDBUtil.cfDeleteIfExist(trace.getTraceId());
                     } catch (RocksDBException e) {
-                        log.info("定时任务删除废弃rocksdb数据:{},{}", trace.getTraceId(), e);
+                        log.info("[USER_SESSION_MAP]定时任务删除废弃rocksdb数据:{},{}", trace.getTraceId(), e);
                     }
                 }
                 HistoryServiceImpl.USER_SESSION_MAP.remove(entry.getKey());
