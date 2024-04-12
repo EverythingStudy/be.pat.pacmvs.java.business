5f8488e8ed7230f0ebb5e723403fa5499b0e4c27
wangmengyao <1>
2024-04-12 16:45:05 +0800

    补充接口返回值

--- src/main/java/cn/staitech/anno/service/impl/BlurImageServiceImpl.java
+++ src/main/java/cn/staitech/anno/service/impl/BlurImageServiceImpl.java
@@ -2,6 +2,7 @@ package cn.staitech.anno.service.impl;
 
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.util.ObjectUtil;
+import cn.staitech.anno.config.MapConstant;
 import cn.staitech.anno.domain.BlurImage;
 import cn.staitech.anno.mapper.BlurImageMapper;
 import cn.staitech.anno.service.BlurImageService;
@@ -69,6 +70,10 @@ public class BlurImageServiceImpl extends ServiceImpl<BlurImageMapper, BlurImage
                 ImageVagueListOutVO outVO = new ImageVagueListOutVO();
                 BeanUtils.copyProperties(image, outVO);
                 outVO.setFuzzyProportion(image.getFuzzyChunk() + "/" + image.getFuzzyCountChunk());
+                outVO.setImageId(image.getImageId().longValue());
+                outVO.setTopicId(image.getTopicId().longValue());
+                outVO.setOrganizationId(image.getOrganizationId().longValue());
+                outVO.setOrganizationName(MapConstant.getOrganizationName(outVO.getOrganizationId()));
                 return outVO;
             }).collect(Collectors.toList());
         }
