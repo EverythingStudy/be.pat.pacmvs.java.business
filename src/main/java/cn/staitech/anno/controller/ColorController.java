e23ae87fd616520fabee93833c435f7abe829820
wanglibei <wanglb@staitech.cn>
2024-09-11 14:17:59 +0800

    增加标签颜色

---
+++ src/main/java/cn/staitech/anno/controller/ColorController.java
@@ -0,0 +1,65 @@
+package cn.staitech.anno.controller;
+
+import cn.staitech.anno.constant.Container;
+import cn.staitech.anno.domain.Color;
+import cn.staitech.anno.service.ColorService;
+import cn.staitech.anno.utils.LanguageUtils;
+import cn.staitech.common.core.domain.R;
+import cn.staitech.common.log.annotation.Log;
+import cn.staitech.common.log.enums.BusinessType;
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @author: wangfeng
+ * @create: 2023-09-13 18:07:38
+ * @Description: 颜色
+ */
+@Api(value = "颜色", tags = "颜色")
+@RestController
+@RequestMapping("/color")
+@Slf4j
+public class ColorController {
+
+    @Resource
+    private ColorService colorService;
+
+    /**
+     * 颜色类型列表 .
+     */
+    @ApiOperationSupport(author = "wangfeng")
+    @ApiOperation(value = "颜色类型列表", notes = "颜色类型列表")
+    @Log(title = "颜色类型列表", menu = "颜色类型列表", subMenu = "颜色类型列表", businessType = BusinessType.QUERY)
+    @GetMapping("/colorType")
+    public R<Map<Integer, String>> colorType() {
+        Map<Integer, String> map;
+        if (LanguageUtils.isEn()) {
+            map = Container.COLOR_TYPE_EN;
+        } else {
+            map = Container.COLOR_TYPE;
+        }
+        return R.ok(map);
+    }
+
+    /**
+     * 颜色列表 .
+     */
+    @ApiOperationSupport(author = "wangfeng")
+    @ApiOperation(value = "颜色类型", notes = "颜色类型 - 王峰")
+    @Log(title = "颜色类型", menu = "颜色类型", subMenu = "颜色类型", businessType = BusinessType.QUERY)
+    @GetMapping("/list")
+    public R<List<Color>> list() throws ExecutionException, InterruptedException {
+        List<Color> list = colorService.list();
+        return R.ok(list);
+    }
+}
---
+++ src/main/java/cn/staitech/anno/mapper/ColorMapper.java
@@ -0,0 +1,14 @@
+package cn.staitech.anno.mapper;
+
+import cn.staitech.anno.domain.Color;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author: wangfeng
+ * @create: 2023-09-10 13:08:31
+ * @Description: 颜色Mapper
+ */
+
+public interface ColorMapper extends BaseMapper<Color> {
+
+}
