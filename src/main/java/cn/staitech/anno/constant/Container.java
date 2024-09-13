93d07157ac9622755fa485c52437732dd6502d6f
lifan <lifan@>
2024-09-13 10:31:11 +0800

    删除不用代码

--- src/main/java/cn/staitech/anno/constant/Container.java
+++ src/main/java/cn/staitech/anno/constant/Container.java
@@ -15,11 +15,6 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Container {
 
     /**
-     * FileUpload - 定义一个基于多线程 的 hashmap
-     */
-    public static final ConcurrentHashMap<String, ConcurrentHashSet<Integer>> FILE_MAP = new ConcurrentHashMap<>(16);
-
-    /**
      * Image - 原始切片 - 切片状态：0上传中、1上传失败、2解析中、3解析失败、4可用
      */
     public static final Map<Integer, String> IMAGE_STATUS_MAP = new ImmutableMap.Builder<Integer, String>()
@@ -41,60 +36,6 @@ public class Container {
             .put(3, "Unavailable")
             .put(4, "Available")
             .put(5, "NotAvailable")
-            .build();
-    
-    
-    /**
-     * 专题用户状态 - EN：0开启，1禁用 SpecialRoleUser
-     */
-    public static final Map<Long, String> SPECIAL_ROLE_STATUS_MAP_EN = new ImmutableMap.Builder<Long, String>()
-            .put(0L, "ON")
-            .put(1L, "FORBIDDEN")
-            .build();
-    
-    /**
-     * 专题用户状态：0开启，1禁用 SpecialRoleUser
-     */
-    public static final Map<Long, String> SPECIAL_ROLE_STATUS_MAP = new ImmutableMap.Builder<Long, String>()
-            .put(0L, "开启")
-            .put(1L, "禁用")
-            .build();
-
-    /**
-     * 项目状态
-     */
-    public static final Map<Integer, String> PROJECT_STATUS = new ImmutableMap.Builder<Integer, String>()
-            .put(1, "待启动")
-            .put(2, "进行中")
-            .put(3, "暂停")
-            .put(4, "已完成")
-            .build();
-
-    /**
-     * 项目状态 - EN
-     */
-    public static final Map<Integer, String> PROJECT_STATUS_EN = new ImmutableMap.Builder<Integer, String>()
-            .put(1, "Pending Started")
-            .put(2, "In process")
-            .put(3, "Pause")
-            .put(4, "Done")
-            .build();
-
-
-    /**
-     * 单审状态
-     */
-    public static final Map<Integer, String> SELF_REVIEW_STATUS = new ImmutableMap.Builder<Integer, String>()
-            .put(1, "未审")
-            .put(2, "已审")
-            .build();
-
-    /**
-     * 单审状态 - EN
-     */
-    public static final Map<Integer, String> SELF_REVIEW_STATUS_EN = new ImmutableMap.Builder<Integer, String>()
-            .put(1, "Unreviewed")
-            .put(2, "Reviewed")
             .build();
 
     /**
@@ -148,28 +89,5 @@ public class Container {
             .put(20, "Other")
             .put(21, "Argyrophilic staining")
             .build();
-
-    /**
-     * 眼科-提示语
-     */
-    public static final Map<Integer, String> EYE_PROMPT_MAP = new ImmutableMap.Builder<Integer, String>()
-            .put(1, "算法要求图像数量≥3张，≤8张。请删除后重新上传。")
-            .put(2, "因图像命名不符合要求，未识别到主图，请在原始切片中手动设置主图")
-            .put(3, "算法要求图像数量≥3张，≤8张。请删除后重新上传。")
-            .build();
-
-
-    /**
-     * 眼科-提示语-en
-     */
-    public static final Map<Integer, String> EYE_PROMPT_MAP_EN = new ImmutableMap.Builder<Integer, String>()
-            .put(1, "算法要求图像数量≥3张，≤8张。请删除后重新上传。")
-            .put(2, "因图像命名不符合要求，未识别到主图，请在原始切片中手动设置主图en")
-            .put(3, "算法要求图像数量≥3张，≤8张。请删除后重新上传。")
-            .build();
-    /**
-     * 眼科ZIP压缩包解压后可解析的图像文件
-     */
-    public static final ImmutableSet<String> IMAGE_EXT_SET = ImmutableSet.of("png", "jpg");
 
 }
