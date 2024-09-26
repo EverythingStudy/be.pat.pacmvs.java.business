1ca120b33ca2e34da88f2166bd4dca56dd297525
wanglibei <wanglb@staitech.cn>
2024-09-26 10:10:23 +0800

    用户手册

---
+++ src/main/java/cn/staitech/anno/domain/UserManual.java
@@ -0,0 +1,59 @@
+package cn.staitech.anno.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * <p>
+ * 用户手册
+ * </p>
+ *
+ * @author wanglibei
+ * @since 2024-01-04
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("tb_user_manual")
+@ApiModel(value = "UserManual对象", description = "用户手册")
+public class UserManual implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "主键ID")
+    @TableId(value = "user_manual_id", type = IdType.AUTO)
+    private Long userManualId;
+
+    @ApiModelProperty(value = "用户手册地址")
+    private String userManualUrl;
+
+    @ApiModelProperty(value = "语种类型 1:中文；2:英文")
+    private Integer messagesType;
+
+    @ApiModelProperty(value = "组织机构ID")
+    private Long organizationId;
+
+    @ApiModelProperty(value = "默认为0，1为删除")
+    private Integer delFlag;
+
+    @ApiModelProperty(value = "创建者ID")
+    private Long createBy;
+
+    @ApiModelProperty(value = "更新者ID")
+    private Long updateBy;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    private Date updateTime;
+
+
+}
---
+++ src/main/java/cn/staitech/anno/mapper/UserManualMapper.java
@@ -0,0 +1,16 @@
+package cn.staitech.anno.mapper;
+
+import cn.staitech.anno.domain.UserManual;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户手册 Mapper 接口
+ * </p>
+ *
+ * @author wanglibei
+ * @since 2024-01-04
+ */
+public interface UserManualMapper extends BaseMapper<UserManual> {
+
+}
